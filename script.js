ccf525973f50f9823dcab23667e37e294026a8e2
enricoisidori <86713366+enricoisidori@users.noreply.github.com>
2023-02-12 14:49:51 +0100

    inizio

---
+++ script.js
@@ -0,0 +1,17 @@
+// let mapwidth = 5000;
+// let mapheight = 5000;
+// let xpos = 400;
+// let ypos = 400;
+// let x = clientX;
+// let y = clientY;
+
+// $(document).ready(function () {
+//   $(this).scrollTo(mapwidth / 2, mapheight / 2);
+// });
+
+// object.onmousemove = function () {
+//   window.scrollX(x + 400);
+//   window.scrollY(y + 400);
+// };
+
+
